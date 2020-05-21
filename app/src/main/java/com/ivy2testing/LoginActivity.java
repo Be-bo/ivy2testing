c2db29ee028cec6870de5d74f3418c673ef79b1b
Zahra <zahra.ghavasieh@ucalgary.ca>
2020-05-21 12:35:55 -0600

    Initial commit. Connected activity to layout and wrote out todos

--- app/src/main/java/com/ivy2testing/LoginActivity.java
+++ app/src/main/java/com/ivy2testing/LoginActivity.java
@@ -1,9 +1,69 @@
 package com.ivy2testing;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-    //Zahra
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-    //TODO: input checking, no design just basic elements for now -> connect with the activity, logging in auth
+import com.google.firebase.auth.FirebaseAuth;
+
+/** @author = Zahra Ghavasieh
+ * Overview: First activity user encounters when launching app
+ * Features: realtime fields check, using Firebase auth for authentication
+*/
+public class LoginActivity extends AppCompatActivity {
+
+    // Constants
+    private static final String TAG = "LoginActivity";
+
+    // Views
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private Button mLoginButton;
+    private TextView mSignUpView;
+
+    // Firebase
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+
+
+/* Override Methods
+***************************************************************************************************/
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+/* OnClick Methods
+***************************************************************************************************/
+
+    // mLoginButton onClick method
+    public void login(View view) {
+    }
+
+    // mSgignUpView onClick method
+    public void signup(View view) {
+        startActivity(new Intent(this, StudentSignUpActivity.class));
+    }
+
+
+/* Input Checking Methods
+***************************************************************************************************/
+    //TODO: input checking: .setError(), email.trim()
+
+    //TODO: disable button if conditions not set (TextWatcher)
+
+
+/* Firebase related Methods
+***************************************************************************************************/
+    //TODO: sign in to Firebase
+
 
 }
