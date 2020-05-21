f57668e51a199e56bc8684a415c052f47473283d
Bebo <rfiker@ucalgary.ca>
2020-05-21 11:34:05 -0600

    TODOs Added

--- app/src/main/java/com/ivy2testing/LoginActivity.java
+++ app/src/main/java/com/ivy2testing/LoginActivity.java
@@ -1,4 +1,9 @@
 package com.ivy2testing;
 
 public class LoginActivity {
+
+    //Zahra
+
+    //TODO: input checking, no design just basic elements for now -> connect with the activity, logging in auth
+
 }
--- app/src/main/java/com/ivy2testing/StudentSignUpActivity.java
+++ app/src/main/java/com/ivy2testing/StudentSignUpActivity.java
@@ -1,4 +1,8 @@
 package com.ivy2testing;
 
 public class StudentSignUpActivity {
+
+    //Clyde
+
+    //TODO: input checking, no design just basic elements for now -> connect with the activity, pushing the data into Firestore
 }
