985991cb3d0d7dbab9ff76d6ed4f53f825a319c6
Zahra <zahra.ghavasieh@ucalgary.ca>
2020-05-22 13:48:02 -0600

    Done basics of organization sign up

--- app/src/main/java/com/ivy2testing/OrganizationSignUpActivity.java
+++ app/src/main/java/com/ivy2testing/OrganizationSignUpActivity.java
@@ -1,6 +1,18 @@
 package com.ivy2testing;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OrganizationSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organization_signup);
+        //setHandlers();
+        //setListeners();
+    }
+
 }
