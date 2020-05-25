23f263803d573582431476a07e06cb4612626f4e
Zahra <zahra.ghavasieh@ucalgary.ca>
2020-05-25 10:01:47 -0600

    Gradient background looks much better now. Used an off-white background so it's easier on the eyes. (also it's the default background colour)

--- app/src/main/java/com/ivy2testing/LoginActivity.java
+++ app/src/main/java/com/ivy2testing/LoginActivity.java
@@ -72,13 +72,6 @@ public class LoginActivity extends AppCompatActivity {
         getDomains();
     }
 
-    @Override
-    public void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        Window window = getWindow();
-        window.setFormat(PixelFormat.RGBA_8888);
-    }
-
 
 /* Initialization Methods
 ***************************************************************************************************/
