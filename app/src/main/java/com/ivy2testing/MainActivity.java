26d958efc3ccb98a5f024eb3d698379883123c16
Zahra <zahra.ghavasieh@ucalgary.ca>
2020-05-26 14:03:29 -0600

    Basic user profile fragment and bottom navigation bar created.

--- app/src/main/java/com/ivy2testing/MainActivity.java
+++ app/src/main/java/com/ivy2testing/MainActivity.java
@@ -1,11 +1,16 @@
 package com.ivy2testing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ivy2testing.userProfile.UserProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
 //        // just immediately jumping into the student sign up page to test it
 //        Intent intent = new Intent(this, StudentSignUpActivity.class);
 //        startActivity(intent);
+
+        // Test bottom navigation
+        mainTabBar = findViewById(R.id.main_tab_bar);
+        mainTabBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.tab_bar_chat:
+                        Toast.makeText(MainActivity.this,"chat!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.tab_bar_home:
+                        Toast.makeText(MainActivity.this,"home!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.tab_bar_profile:
+                        selectedFragment = new UserProfileFragment();
+                        break;
+                }
+                if (selectedFragment!= null)
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main_fragmentContainer, selectedFragment).commit();
+
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/ivy2testing/userProfile/UserProfileFragment.java
@@ -0,0 +1,27 @@
+package com.ivy2testing.userProfile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ivy2testing.R;
+
+public class UserProfileFragment extends Fragment {
+
+
+    public UserProfileFragment(){
+        // Required Empty public constructor
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_userprofile, container, false);
+    }
+}
