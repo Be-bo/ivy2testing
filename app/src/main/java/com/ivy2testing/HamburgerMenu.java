e7ca30c6a9aad24ab1487a67e85c062b926a6a6c
Dr. Blyde <captainclydeb@hotmail.com>
2020-05-28 17:05:40 -0600

    Buttons Working prototype
    They can be switched by being enabled or disabled but the solution is very inelegant.
    This is really just to help visualize whats going on.

--- app/src/main/java/com/ivy2testing/HamburgerMenu.java
+++ app/src/main/java/com/ivy2testing/HamburgerMenu.java
@@ -20,12 +20,15 @@ import androidx.drawerlayout.widget.DrawerLayout;
 public class HamburgerMenu extends AppCompatActivity {
     private DrawerLayout drawer;
 
+    private Button currentButton;
 
+    private Button button_1;
+    private Button button_2;
+    private Button button_3;
+    private Button button_4;
+    private Button button_5;
+    private Button button_6;
 
-    private Button addBtn;
-    private ConstraintLayout constLayout;
-    private LinearLayout bblContainer;
-    private Button Ucalgary;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,11 +38,56 @@ public class HamburgerMenu extends AppCompatActivity {
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
 
-        //testing some bs
-        addBtn = findViewById(R.id.button2);
-        constLayout = findViewById(R.id.constraintlayout);
-        bblContainer = findViewById(R.id.bubble_container);
-        Ucalgary = findViewById(R.id.button);
+        //testing some buttons
+        button_1 = findViewById(R.id.btn_1);
+        button_1.setEnabled(false);
+        currentButton = button_1;
+        button_1.setTextColor(getResources().getColor(R.color.android_default_bg));
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateButton(button_1);
+            }
+        });
+        button_2 = findViewById(R.id.btn_2);
+        button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateButton(button_2);
+            }
+        });
+        button_3 = findViewById(R.id.btn_3);
+        button_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateButton(button_3);
+            }
+        });
+        button_4 = findViewById(R.id.btn_4);
+        button_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateButton(button_4);
+            }
+        });
+        button_5 = findViewById(R.id.btn_5);
+        button_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateButton(button_5);
+            }
+        });
+        button_6 = findViewById(R.id.btn_6);
+        button_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateButton(button_6);
+            }
+        });
+
+
+
+
 
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
@@ -51,40 +99,16 @@ public class HamburgerMenu extends AppCompatActivity {
         getSupportActionBar().setTitle(null);
         //set color of draw bar
         toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.interaction));
-        final Button newButton = new Button(this);
-        newButton.setText("Hey Im new around here");
-
-
-
-        addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(HamburgerMenu.this, "suuup homeslice", Toast.LENGTH_SHORT).show();
-
-                //bblContainer.addView(newButton);
-                buttonCreator();
-            }
-        });
-        Ucalgary.setTextColor(getResources().getColor(R.color.white));
-        Ucalgary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(HamburgerMenu.this, "UCALGARYY", Toast.LENGTH_SHORT).show();
-                Ucalgary.setEnabled(false);
-                Ucalgary.setTextColor(getResources().getColor(R.color.interaction));
-            }
-        });
 
     }
-    private void buttonCreator(){
-        ContextThemeWrapper newContext = new ContextThemeWrapper(getBaseContext(), R.style.Bubble);
-        Button newButton2 = new Button(newContext);
-        bblContainer.addView(newButton2);
-        newButton2.setText("Hey Im new around here2");
-        bblContainer.addView(newButton2);
-
-
+    public void updateButton(Button b) {
+        currentButton.setEnabled(true);
+        currentButton.setTextColor(getResources().getColor(R.color.interaction));
+        b.setEnabled(false);
+        b.setTextColor(getResources().getColor(R.color.android_default_bg));
+        currentButton = b;
     }
+
 
     @Override
     public void onBackPressed() {
@@ -95,4 +119,5 @@ public class HamburgerMenu extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
 }
