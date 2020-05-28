fae2a41a6c467914c980d4e248ade5a96844f620
Dr. Blyde <captainclydeb@hotmail.com>
2020-05-27 21:34:49 -0600

    Update HamburgerMenu.java

--- app/src/main/java/com/ivy2testing/HamburgerMenu.java
+++ app/src/main/java/com/ivy2testing/HamburgerMenu.java
@@ -22,7 +22,6 @@ public class HamburgerMenu extends AppCompatActivity {
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
 
-
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
