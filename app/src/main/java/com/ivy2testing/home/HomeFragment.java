06825dda60db97f752f2f74df9913502e6f09bc7
Dr. Blyde <captainclydeb@hotmail.com>
2020-06-17 15:03:35 -0600

    For merge to Master

--- app/src/main/java/com/ivy2testing/home/HomeFragment.java
+++ app/src/main/java/com/ivy2testing/home/HomeFragment.java
@@ -3,6 +3,7 @@ package com.ivy2testing.home;
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +26,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
@@ -49,23 +51,26 @@ public class HomeFragment extends Fragment implements FeedAdapter.FeedViewHolder
     private Context mContext;
     private View rootView;
 
-    private Button uni_button;
-    private Button current_button;
-
+    // firebase
     private FirebaseFirestore db_reference = FirebaseFirestore.getInstance();
     private StorageReference db_storage = FirebaseStorage.getInstance().getReference();
 
+    // User View Model
     private Student student;
     private boolean is_organization = false;
     private UserViewModel this_user_viewmodel;
 
 
+    // main feed
     private final ArrayList<Post> post_arraylist = new ArrayList<Post>();
 
     private RecyclerView feed_recycler_view;
     private RecyclerView.Adapter feed_adapter;
     private RecyclerView.LayoutManager feed_layout_manager;
 
+
+
+    // bubbles
     private final ArrayList<String> bubble_arraylist = new ArrayList<String>();
 
     private RecyclerView bubble_recycler_view;
@@ -117,14 +122,12 @@ public class HomeFragment extends Fragment implements FeedAdapter.FeedViewHolder
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        // Initialization
-       // uni_button = rootView.findViewById(R.id.btn_1);
 
 
-        //current_button = uni_button;
-       // current_button.setEnabled(false);
 
-        // recycler view
+
+
+        // bubbles recycler view
 
 
         bubble_arraylist.add("University");
@@ -148,124 +151,39 @@ public class HomeFragment extends Fragment implements FeedAdapter.FeedViewHolder
 
 
 
-
+        // feed recycler view
         BuildArrayList();
-
-//        Toast.makeText(mContext, ""+post_arraylist.get(0).getAuthor_name(), Toast.LENGTH_SHORT).show();
 
         feed_recycler_view = rootView.findViewById(R.id.feed_recycler_view);
         feed_recycler_view.setHasFixedSize(true);
-        //TODO not sure required context here
+   /*     //TODO not sure required context here
         feed_layout_manager = new LinearLayoutManager(getContext());
         feed_adapter = new FeedAdapter(post_arraylist, this);
         feed_recycler_view.setLayoutManager(feed_layout_manager);
         feed_recycler_view.setAdapter(feed_adapter);
+*/
+
+        feed_recycler_view.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+               // recyclerView.
+                if (!recyclerView.canScrollVertically(1)) {
+                   // Toast.makeText(getContext(), "Last" + recyclerView.computeVerticalScrollExtent(), Toast.LENGTH_LONG).show();
+                   // PullMorePosts();
+                }
+            }
+        });
+
+
 
         return rootView;
     }
 
-
-
-    /* ************************************************************************************************** */
-    // resets main "Uni" button to be selected/ deselects others,
-    // this method is can be called when returning from other activities or back presses
-
-    private void resetMainBubble() {
-        current_button.setEnabled(true);
-        uni_button.setEnabled(false);
-        current_button = uni_button;
-    }
-
-    /* ************************************************************************************************** */
-    // the buttons are set to enabled to be clickable and disabled  when selected so they can't be constantly clicked
-    // they can be set to selected and unselected but it depends on what function they perform when clicked
-    // I was struggling to get the buttons to stay clicked so i threw together this enabled solution, and it works,
-    // but we can change the states... for now this works
-
-    // all bubbles are also set with an onClick function so they call this function, and locally, whichever button
-    // triggers the function, is saved locally and the view is updated. current_button = most recently clicked button, the last button will be re-enabled
-    // functions can be placed here to perform a function based off the text/ information in a button i.e a search with current_button.getText
-
-    public void toggleEnabled(View view) {
-        current_button.setEnabled(true);
-        view.setEnabled(false);
-        current_button = (Button) view;
-        //  Toast.makeText(this, "" + current_button.getText(), Toast.LENGTH_SHORT).show();
-
-    }
-
-    /* ************************************************************************************************** */
-    // this function navigates to the collection containing posts, checks if an item is a post. It then converts the
-    // item to a hash map and creates a post in the fragment container view
-
-    private void buildMainFeed() {
-        //DocumentReference sampler = db_reference.collection("universities").document("ucalgary.ca").collection("posts").document("5c2fbbf4-c06b-406c-92e9-71321f046d43");
-        db_reference.collection("universities").document("ucalgary.ca").collection("posts")
-                .whereEqualTo("is_event", false)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                // Toast.makeText(MainActivity.this, document.getId() + " => " + document.getData(), Toast.LENGTH_SHORT).show();
-                                Map<String, Object> is_post = document.getData();
-                                postCreator(is_post);
-                            }
-                        } else {
-                            //  Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
-    }
-
-    /* ************************************************************************************************** */
-    //This function takes a hash map and fills a sample post with its information
-    //https://stackoverflow.com/questions/6216547/android-dynamically-add-views-into-view
-
-    private void postCreator(Map<String, Object> post) {
-        // layout inflater puts views inside other views
-        LayoutInflater vi = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View v = vi.inflate(R.layout.sample_post, null);
-
-        // fill in any details dynamically here
-        final ImageView sample_image = v.findViewById(R.id.sample_image);
-        //TODO do posts have titles?
-        TextView tv = v.findViewById(R.id.sample_name);
-        TextView description = v.findViewById(R.id.sample_description);
-        TextView author_name = v.findViewById(R.id.sample_author_nam);
-        description.setText((String) post.get("text"));
-        author_name.setText((String) post.get("author_name"));
-        String visual_path = (String) post.get("visual");
-        // if the visual path contains a / it will be a link to storage location
-        if (visual_path.contains("/")) {
-            // max download size = 1.5mb
-            db_storage.child(visual_path).getBytes(1500000).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-                @Override
-                public void onSuccess(byte[] bytes) {
-
-                    // bytes is an byte [] returned from storage,
-                    // set the image to be visible
-                    sample_image.setImageBitmap(BitmapFactory.decodeByteArray(bytes, 0, bytes.length));
-                    sample_image.setVisibility(View.VISIBLE);
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    // TODO error handler
-                }
-            });
-        }
-
-
-        // insert into main view
-        // currently inserts newest post last... needs better queries
-       /* ViewGroup insertPoint = rootView.findViewById(R.id.main_fragment_linear_layout);
-        insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));*/
-    }
     private void BuildArrayList(){
         db_reference.collection("universities").document("ucalgary.ca").collection("posts")
-                .limit(25)
+                .limit(10)
+                .orderBy("creation_millis", Query.Direction.DESCENDING)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -287,9 +205,35 @@ public class HomeFragment extends Fragment implements FeedAdapter.FeedViewHolder
                         }
                     }
                 });
+    }
 
-
-
+    private void PullMorePosts(){
+        db_reference.collection("universities").document("ucalgary.ca").collection("posts")
+                .limit(10)
+                .orderBy("creation_millis", Query.Direction.DESCENDING)
+               // .startAt(post_arraylist.get(post_arraylist.size()-1).getCreation_millis() +1 )
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if(task.getResult()!=null) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    // Toast.makeText(MainActivity.this, document.getId() + " => " + document.getData(), Toast.LENGTH_SHORT).show();
+                                    // Log.d(TAG, "onComplete: " + document.getData().toString());
+                                    //TODO THESE ARE SAVED AS EVENTS
+                                    Post event_object = document.toObject(Event.class);
+                                    post_arraylist.add(event_object);
+                                }
+                                Toast.makeText(mContext, ""+post_arraylist.size(), Toast.LENGTH_SHORT).show();
+                                feed_adapter.notifyDataSetChanged();
+                            }
+                        }
+                        else {
+                            //  Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
     private void buildEventFeed(){
 
@@ -299,6 +243,8 @@ public class HomeFragment extends Fragment implements FeedAdapter.FeedViewHolder
         feed_recycler_view.setLayoutManager(feed_layout_manager);
         feed_recycler_view.setAdapter(feed_adapter);
     }
+
+
 
     @Override
     public void onFeedClick(int position) {
@@ -310,11 +256,60 @@ public class HomeFragment extends Fragment implements FeedAdapter.FeedViewHolder
 
     @Override
     public void onBubbleClick(int position) {
-        Toast.makeText(mContext, ""+bubble_arraylist.get(position), Toast.LENGTH_SHORT).show();
+       // Toast.makeText(mContext, ""+bubble_arraylist.get(position), Toast.LENGTH_SHORT).show();
+        post_arraylist.clear();
+        feed_adapter.notifyDataSetChanged();
+        if(bubble_arraylist.get(position).equals("Posts")){
+           BubbleQuery(false);
+        }
+        else if(bubble_arraylist.get(position).equals("Events")){
+            BubbleQuery(true);
+
+        }
+        else if(bubble_arraylist.get(position).equals("Univeristy")){
+            PullMorePosts();
+        }
+
+        //BubbleQuery();
+
 
         //TODO THIS IS WHERE TO HANDLE BUBBLE CLICKS
 
         // bubble_arraylist.get(position); <- this is the clicked bubble
+    }
 
+    private void BubbleQuery(Boolean event){
+
+
+        db_reference.collection("universities").document("ucalgary.ca").collection("posts")
+                .limit(10)
+                .whereEqualTo("is_event",event)
+                //TODO if using the order by in this area, it requires an index to be set manually, or with (start from)
+                //.orderBy("creation_millis", Query.Direction.DESCENDING)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(mContext, ""+ post_arraylist.size(), Toast.LENGTH_SHORT).show();
+                            if(task.getResult()!=null) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    // Toast.makeText(MainActivity.this, document.getId() + " => " + document.getData(), Toast.LENGTH_SHORT).show();
+                                    // Log.d(TAG, "onComplete: " + document.getData().toString());
+                                    //TODO THESE ARE SAVED AS EVENTS
+                                    Post event_object = document.toObject(Event.class);
+                                    post_arraylist.add(event_object);
+                                   // Toast.makeText(mContext, ""+ post_arraylist.size(), Toast.LENGTH_SHORT).show();
+                                }
+
+
+                                feed_adapter.notifyDataSetChanged();
+                            }
+                        }
+                        else {
+                              Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }
